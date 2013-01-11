c955f29eb58d10e638c4685524aed162ccba5a23
Mike C <mcrosson_git at nusku dot net>
2013-01-11 16:35:42 -0500

    Updated helper script

--- update_addons.sh
+++ update_addons.sh
@@ -2,5 +2,10 @@
 
 git submodule update
 python repo_prep.py
-for file in `find ./ -iname *.zip | grep -v repo/`; do mv $file repo/; done
+for dir in `git submodule | cut -d' ' -f3`; do cp -fr $dir repo/; done
+cp -fr repository.nuskunetworks repo/
+for item in `find ./repo/ -iname .git`; do rm -rf $item ; done
+cd repo/repository.nuskunetworks
+rm -rf .git
+cd ../../
 
