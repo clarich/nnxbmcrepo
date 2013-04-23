ebe6849c84c8a625a9461effe792b44cf8f48a94
Mike C <mcrosson_git at nusku dot net>
2013-04-22 23:30:22 -0400

    Bumped versions of add-ons

--- repo/plugin.program.com.netflix.mediaclient/default.py
+++ repo/plugin.program.com.netflix.mediaclient/default.py
@@ -12,4 +12,4 @@ __version__    = __addon__.getAddonInfo('version')
 __language__   = __addon__.getLocalizedString
 
 if ( __name__ == "__main__" ):
-	xbmc.executebuiltin('XBMC.StartActivity("com.netflix.mediaclient")')
+	xbmc.executebuiltin('XBMC.StartAndroidActivity("com.netflix.mediaclient")')
